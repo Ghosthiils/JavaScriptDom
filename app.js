dbb6b2fd58064f8c829235e5fcbc47f707d431a9
Luiz Fernando <luizfranca35mc@gmail.com>
2025-06-26 22:07:24 -0300

    JavaScripty incompleto

---
+++ app.js
@@ -0,0 +1,27 @@
+function fnMudar () {
+    document.getElementById("titulo").innerHTML = "<em>Xavier, Guto</em>"
+    document.getElementById("titulo").style.color = "red"
+}
+function fnModoDark() {
+    document.getElementById("pagina").style.background = "black"
+    document.getElementById("pagina").style.color = "white"
+}
+
+function fnModoLight() {
+    document.getElementById("pagina").style.background = "white"
+    document.getElementById("pagina").style.color = "black"
+
+
+}
+
+function fnModofesta() {
+    document.getElementById("pagina").style.background = document.body.style.background = "linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)";
+    document.getElementById("pagina").style.color = "black"
+
+}
+
+let fotosbanner = ['banner.jpg' , 'banner2.webp' , 'banner3.jpg']
+
+function FotoAtual() {
+    document.getElementById
+}
